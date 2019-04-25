4353580acd1274b24b5d65c250fb8d5fc5f504c5
VenkataSambasivaRao <sambakesanam@gmail.com>
2019-04-25 14:00:49 +0400

    Final Changes

--- Dialogs/BasicLuisDialog.cs
+++ Dialogs/BasicLuisDialog.cs
@@ -111,8 +111,7 @@ namespace Microsoft.Bot.Sample.LuisBot
             email = response;
 
             await context.PostAsync($@"Thank you for your interest, your request has been logged. Our customer service team will get back to you shortly.
-                                    {Environment.NewLine}Your service request  summary:
-                                   
+                                    {Environment.NewLine}Your service request  summary:                                 
                                     {Environment.NewLine}Complaint Title: {complaint},
                                     {Environment.NewLine}Customer Name: {customerName},
                                     {Environment.NewLine}Phone Number: {phone},
@@ -148,6 +147,279 @@ namespace Microsoft.Bot.Sample.LuisBot
             await context.PostAsync(feedback);
 
             context.Wait(AnythingElseHandler);
+        }
+        [LuisIntent("LOCATION")]
+        public async Task LOCATION(IDialogContext context, LuisResult result)
+        {
+            EntityRecommendation employeeName;
+
+            string name = string.Empty;
+
+            if (result.TryFindEntity("Location", out employeeName))
+            {
+                name = employeeName.Entity;
+
+                var message = context.MakeMessage();
+
+                var attachment = GetSelectedCard(name);
+                message.Attachments.Add(attachment);
+
+                await context.PostAsync(message);
+
+                var feedback = ((Activity)context.Activity).CreateReply("Is there anything else that I could help?");
+
+                feedback.SuggestedActions = new SuggestedActions()
+                {
+                    Actions = new List<CardAction>()
+                {
+                    //new CardAction(){ Title = "👍", Type=ActionTypes.PostBack, Value=$"yes-positive-feedback" },
+                    //new CardAction(){ Title = "👎", Type=ActionTypes.PostBack, Value=$"no-negative-feedback" }
+
+                     new CardAction(){ Title = "Yes", Type=ActionTypes.PostBack, Value=$"Yes" },
+                    new CardAction(){ Title = "No", Type=ActionTypes.PostBack, Value=$"No" }
+                }
+                };
+
+                await context.PostAsync(feedback);
+
+                context.Wait(AnythingElseHandler);
+            }
+            else
+            {
+                PromptDialog.Text(
+                    context: context,
+                    resume: DisplaySelectedCard,
+                    prompt: "Please let me know your location preference?",
+                    retry: "Sorry, I don't understand that."
+                            );
+            }
+        }
+        public async Task DisplaySelectedCard(IDialogContext context, IAwaitable<string> result)
+        {
+            var selectedCard = await result;
+
+            var message = context.MakeMessage();
+
+            var attachment = GetSelectedCard(selectedCard);
+            message.Attachments.Add(attachment);
+
+            await context.PostAsync(message);
+
+            var feedback = ((Activity)context.Activity).CreateReply("Is there anything else that I could help?");
+
+            feedback.SuggestedActions = new SuggestedActions()
+            {
+                Actions = new List<CardAction>()
+                {
+                    //new CardAction(){ Title = "👍", Type=ActionTypes.PostBack, Value=$"yes-positive-feedback" },
+                    //new CardAction(){ Title = "👎", Type=ActionTypes.PostBack, Value=$"no-negative-feedback" }
+
+                     new CardAction(){ Title = "Yes", Type=ActionTypes.PostBack, Value=$"Yes" },
+                    new CardAction(){ Title = "No", Type=ActionTypes.PostBack, Value=$"No" }
+                }
+            };
+
+            await context.PostAsync(feedback);
+
+            context.Wait(AnythingElseHandler);
+        }
+        private static Microsoft.Bot.Connector.Attachment GetSelectedCard(string selectedCard)
+        {
+            if (selectedCard.Contains("mankhool") || selectedCard.Contains("Mankhool") || selectedCard.Contains("al mankhool") || selectedCard.Contains("Al Mankhool"))
+            {
+                return GetMankhoolCard();
+            }
+            else if (selectedCard.Contains("barsha") || selectedCard.Contains("Barsha") || selectedCard.Contains("Al Barsha") || selectedCard.Contains("al barsha"))
+            {
+                return GetAlBarshaCard();
+            }
+            else if (selectedCard.Contains("Al Qusais") || selectedCard.Contains("qusais") || selectedCard.Contains("Qusais"))
+            {
+                return GetQusaisCard();
+            }
+            else if (selectedCard.Contains("Nad Al Hamar") || selectedCard.Contains("nad al hamar"))
+            {
+                return GetNadAlHamarCard();
+            }
+            else if (selectedCard.Contains("Al Safa") || selectedCard.Contains("al safa"))
+            {
+                return GetAlSafaCard();
+            }
+            else if (selectedCard.Contains("Al Badaa") || selectedCard.Contains("al badaa"))
+            {
+                return GetAlBadaaCard();
+            }
+            return GetDefaultCard();
+        }
+        private static Microsoft.Bot.Connector.Attachment GetAlBadaaCard()
+        {
+            var heroCard = new HeroCard
+            {
+                Title = "",
+                Subtitle = "",
+                Text = "",
+                Images = new List<CardImage> { new CardImage("https://intertechealthwebapp.azurewebsites.net/AlBadaa.png") },
+                Buttons = new List<CardAction> { new CardAction(ActionTypes.OpenUrl, "Click to see the map", value: "https://www.google.ae/maps/place/Al+Badaa+Health+Centre/@25.2114413,55.2724694,17z/data=!4m8!1m2!2m1!1sAl+Badaa+Health+Center!3m4!1s0x3e5f42e8b1e728f1:0x9386335357fb7552!8m2!3d25.2120592!4d55.2722835") }
+            };
+
+            return heroCard.ToAttachment();
+        }
+        private static Microsoft.Bot.Connector.Attachment GetAlSafaCard()
+        {
+            var heroCard = new HeroCard
+            {
+                Title = "",
+                Subtitle = "",
+                Text = "",
+                Images = new List<CardImage> { new CardImage("https://intertechealthwebapp.azurewebsites.net/AlSafa.png") },
+                Buttons = new List<CardAction> { new CardAction(ActionTypes.OpenUrl, "Click to see the map", value: "https://www.google.ae/maps/place/Al+Safa+Primary+Health+Care+Center/@25.1896,55.2362383,17z/data=!3m1!4b1!4m5!3m4!1s0x3e5f4208f522d93f:0x4c43896c0d306972!8m2!3d25.1896!4d55.238427") }
+            };
+
+            return heroCard.ToAttachment();
+        }
+        private static Microsoft.Bot.Connector.Attachment GetNadAlHamarCard()
+        {
+            var heroCard = new HeroCard
+            {
+                Title = "",
+                Subtitle = "",
+                Text = "",
+                Images = new List<CardImage> { new CardImage("https://intertechealthwebapp.azurewebsites.net/NadAlHamar.png") },
+                Buttons = new List<CardAction> { new CardAction(ActionTypes.OpenUrl, "Click to see the map", value: "https://www.google.ae/maps/place/Nadd+Al+Hamar+Health+Center/@25.199364,55.3745396,16z/data=!4m8!1m2!2m1!1sNad+Al+Hamar+Clinic!3m4!1s0x3e5f6715c4716b27:0xbd16bd0f728ddd3a!8m2!3d25.199364!4d55.378917") }
+            };
+
+            return heroCard.ToAttachment();
+        }
+        private static Microsoft.Bot.Connector.Attachment GetMankhoolCard()
+        {
+            var heroCard = new HeroCard
+            {
+                Title = "",
+                Subtitle = "",
+                Text = "",
+                Images = new List<CardImage> { new CardImage("https://intertechealthwebapp.azurewebsites.net/DMankhool.png") },
+                Buttons = new List<CardAction> { new CardAction(ActionTypes.OpenUrl, "Click to see the map", value: "https://www.google.ae/maps/dir/''/''/@25.2459475,55.2868377,14.83z/data=!4m8!4m7!1m0!1m5!1m1!1s0x3e5f4321e1e53069:0x218a7bfc58fbcf0e!2m2!1d55.2923198!2d25.2466252") }
+            };
+
+            return heroCard.ToAttachment();
+        }
+        private static Microsoft.Bot.Connector.Attachment GetAlBarshaCard()
+        {
+            var heroCard = new HeroCard
+            {
+                Title = "",
+                Subtitle = "",
+                Text = "",
+                Images = new List<CardImage> { new CardImage("https://intertechealthwebapp.azurewebsites.net/DAlBarsha.png") },
+                Buttons = new List<CardAction> { new CardAction(ActionTypes.OpenUrl, "Click to see the map", value: "https://www.google.ae/maps/dir/''/AL+Barsha+Health+Centre+-+Al+Barsha+3+-+Dubai/@25.095679,55.1989079,13.7z/data=!4m8!4m7!1m0!1m5!1m1!1s0x3e5f6b8b20ab6a27:0xc41e11236896cc64!2m2!1d55.2024733!2d25.0975157") }
+            };
+
+            return heroCard.ToAttachment();
+        }
+        private static Microsoft.Bot.Connector.Attachment GetDefaultCard()
+        {
+            var heroCard = new HeroCard
+            {
+                Title = "",
+                Subtitle = "",
+                Text = "",
+                Images = new List<CardImage> { new CardImage("https://intertechealthwebapp.azurewebsites.net/DDHAHeadOffice.png") },
+                Buttons = new List<CardAction> { new CardAction(ActionTypes.OpenUrl, "Click to see the map", value: "https://www.google.ae/maps/dir/''/DHA+HEAD+OFFICE+-+Dubai/@25.243802,55.3158075,13.42z/data=!4m8!4m7!1m0!1m5!1m1!1s0x3e5f5d2967d29a6d:0xcd99cea2aee34140!2m2!1d55.3196429!2d25.2468578") }
+            };
+
+            return heroCard.ToAttachment();
+        }
+        private static Microsoft.Bot.Connector.Attachment GetQusaisCard()
+        {
+            var heroCard = new HeroCard
+            {
+                Title = "",
+                Subtitle = "",
+                Text = "",
+                Images = new List<CardImage> { new CardImage("https://intertechealthwebapp.azurewebsites.net/AlQusais.png") },
+                Buttons = new List<CardAction> { new CardAction(ActionTypes.OpenUrl, "Click to see the map", value: "https://www.google.ae/maps/dir/''/DHA+HEAD+OFFICE+-+Dubai/@25.243802,55.3158075,13.42z/data=!4m8!4m7!1m0!1m5!1m1!1s0x3e5f5d2967d29a6d:0xcd99cea2aee34140!2m2!1d55.3196429!2d25.2468578") }
+            };
+
+            return heroCard.ToAttachment();
+        }
+        [LuisIntent("ENQUIRY")]
+        public async Task ENQUIRY(IDialogContext context, LuisResult result)
+        {
+           
+
+            var feedback = ((Activity)context.Activity).CreateReply("Let us know what are you interested in?");
+
+            feedback.SuggestedActions = new SuggestedActions()
+            {
+                Actions = new List<CardAction>()
+                {
+                    //new CardAction(){ Title = "👍", Type=ActionTypes.PostBack, Value=$"yes-positive-feedback" },
+                    //new CardAction(){ Title = "👎", Type=ActionTypes.PostBack, Value=$"no-negative-feedback" }
+
+                     new CardAction(){ Title = "Appointments", Type=ActionTypes.PostBack, Value=$"Appointments" },
+                    new CardAction(){ Title = "Search Hospitals", Type=ActionTypes.PostBack, Value=$"Search" },
+                    new CardAction(){ Title = "Suggestions and Complaints", Type=ActionTypes.PostBack, Value=$"Suggestions" },
+                    new CardAction(){ Title = "Status", Type=ActionTypes.PostBack, Value=$"Status" }
+                }
+            };
+
+            await context.PostAsync(feedback);
+
+            context.Wait(ResumeTypeSubOptionsAsync);
+        }
+        public virtual async Task ResumeTypeSubOptionsAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
+        {
+            var selection = await argument;
+            if (selection.Text.Contains("Suggestions"))
+            {
+                PromptDialog.Text(
+                 context: context,
+                 resume: CustomerNameHandler,
+                 prompt: "What is your complaint/suggestion?",
+                 retry: "Sorry, I don't understand that the question."
+             );
+            }
+            else if (selection.Text.Contains("Search"))
+            {
+                PromptDialog.Text(
+                context: context,
+                resume: DisplaySelectedCard,
+                prompt: "Please let me know your location preference?",
+                retry: "Sorry, I don't understand that."
+                        );
+            }
+            else if (selection.Text.Contains("Appointments"))
+            {
+                var feedback = ((Activity)context.Activity).CreateReply("Please choose a specialist.");
+
+                feedback.SuggestedActions = new SuggestedActions()
+                {
+                    Actions = new List<CardAction>()
+                {
+                    //new CardAction(){ Title = "👍", Type=ActionTypes.PostBack, Value=$"yes-positive-feedback" },
+                    //new CardAction(){ Title = "👎", Type=ActionTypes.PostBack, Value=$"no-negative-feedback" }
+
+                     new CardAction(){ Title = "Allergist", Type=ActionTypes.PostBack, Value=$"Allergist" },
+                    new CardAction(){ Title = "Cardiologist", Type=ActionTypes.PostBack, Value=$"Cardiologist" },
+                      new CardAction(){ Title = "Dermatologist", Type=ActionTypes.PostBack, Value=$"Dermatologist" },
+                        new CardAction(){ Title = "Family Physician", Type=ActionTypes.PostBack, Value=$"FamilyPhysician" },
+                          new CardAction(){ Title = "Gastro Enterologist", Type=ActionTypes.PostBack, Value=$"GastroEnterologist" },
+                            new CardAction(){ Title = "Pediatrician", Type=ActionTypes.PostBack, Value=$"Pediatrician" }
+                }
+                };
+
+                await context.PostAsync(feedback);
+
+                context.Wait(AppointmentSpecialist);
+            }
+            else if (selection.Text.Contains("Status"))
+            {
+                PromptDialog.Text(
+           context: context,
+           resume: CaseRefStatus,
+           prompt: "Enter the Case Reference Number.",
+           retry: "Didn't get that!");
+            }
         }
         [LuisIntent("STATUS")]
         public async Task CaseStatus(IDialogContext context, LuisResult result)
